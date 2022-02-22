6e281f80fce84ed3b27c1f7b7ccab999c4901263
Didier-Ro <zap363@amerike.edu.mx>
2022-02-22 07:45:19 -0600

    Finished Movement Player 1

--- Assets/[SCRIPTS]/Movement.cs
+++ Assets/[SCRIPTS]/Movement.cs
@@ -8,7 +8,7 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = 0.1f;
+        speed = 10f;
     }
 
     // Update is called once per frame
@@ -17,21 +17,21 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
 
-            transform.Translate(1, 0, 0 * Time.deltaTime * speed);
+            transform.Translate(1 * speed * Time.deltaTime, 0, 0);
 
         }
 
         if (Input.GetKey(KeyCode.A))
         {
 
-            transform.Translate(1, 0, 0 * Time.deltaTime * speed);
+            transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
 
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
 
-            transform.Translate(0, 1, 0 * Time.deltaTime * speed);
+            transform.Translate(0, 3, 0 * Time.deltaTime * speed);
 
         }
     }
