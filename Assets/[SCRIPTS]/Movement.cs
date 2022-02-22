21ebfa1f1f1969832cda34afc48d66631ef22ae2
Didier-Ro <zap363@amerike.edu.mx>
2022-02-22 09:07:48 -0600

    change Comments and Variables

--- Assets/[SCRIPTS]/Movement.cs
+++ Assets/[SCRIPTS]/Movement.cs
@@ -1,38 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        speed = 10f;
-    }
+    [SerializeField] private float _speed = default;
+    [SerializeField] private float _jumpspeed = default;
+    [SerializeField] private float _movement = default;
 
-    // Update is called once per frame
+    
+
     void Update()
     {
         if (Input.GetKey(KeyCode.D))
         {
-
-            transform.Translate(1 * speed * Time.deltaTime, 0, 0);
-
+            Move();
         }
-
         if (Input.GetKey(KeyCode.A))
         {
-
-            transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
-
+            transform.Translate(-1 * _speed * Time.deltaTime, 0, 0);
         }
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
-            transform.Translate(0, 3, 0 * Time.deltaTime * speed);
-
+            transform.Translate(0, 3 * Time.deltaTime * _jumpspeed, 0);
         }
+    }
+
+    void Move()
+    {
+        transform.Translate(1 * _speed * Time.deltaTime, 0, 0);
     }
 }
