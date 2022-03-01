daf4021c51dbcbada3ea88431ddcb1d4571a7eb3
Eva Luna <zap363@amerike.edu.mx>
2022-03-01 07:39:26 -0600

    Change variable names

--- Assets/[SCRIPTS]/Attacks.cs
+++ Assets/[SCRIPTS]/Attacks.cs
@@ -2,63 +2,63 @@ using UnityEngine;
 
 public class Attacks : MonoBehaviour
 {
-     [SerializeField] private GameObject smallFist = default;
-     [SerializeField] private GameObject mediumFist = default;
-     [SerializeField] private GameObject bigFist = default;
+     [SerializeField] private GameObject _smallFist = default;
+     [SerializeField] private GameObject _mediumFist = default;
+     [SerializeField] private GameObject _bigFist = default;
 
      void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            smallFist.SetActive(true);
+            _smallFist.SetActive(true);
         }
         if (Input.GetKeyUp(KeyCode.S))
         {
-            smallFist.SetActive(false);
+            _smallFist.SetActive(false);
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
-            smallFist.SetActive(true);
-            smallFist.GetComponent<BoxCollider2D>().enabled = false;
-            mediumFist.SetActive(true);
+            _smallFist.SetActive(true);
+            _smallFist.GetComponent<BoxCollider2D>().enabled = false;
+            _mediumFist.SetActive(true);
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
-            smallFist.SetActive(false);
-            smallFist.GetComponent<BoxCollider2D>().enabled = true;
-            mediumFist.SetActive(false);
+            _smallFist.SetActive(false);
+            _smallFist.GetComponent<BoxCollider2D>().enabled = true;
+            _mediumFist.SetActive(false);
         }
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            smallFist.SetActive(true);
-            smallFist.GetComponent<BoxCollider2D>().enabled = false;
-            mediumFist.SetActive(true);
-            mediumFist.GetComponent<BoxCollider2D>().enabled = false;
-            bigFist.SetActive(true);
+            _smallFist.SetActive(true);
+            _smallFist.GetComponent<BoxCollider2D>().enabled = false;
+            _mediumFist.SetActive(true);
+            _mediumFist.GetComponent<BoxCollider2D>().enabled = false;
+            _bigFist.SetActive(true);
         }
         if (Input.GetKeyUp(KeyCode.Q))
         {
-            smallFist.SetActive(false);
-            smallFist.GetComponent<BoxCollider2D>().enabled = true;
-            mediumFist.SetActive(false);
-            mediumFist.GetComponent<BoxCollider2D>().enabled = true;
-            bigFist.SetActive(false);
+            _smallFist.SetActive(false);
+            _smallFist.GetComponent<BoxCollider2D>().enabled = true;
+            _mediumFist.SetActive(false);
+            _mediumFist.GetComponent<BoxCollider2D>().enabled = true;
+            _bigFist.SetActive(false);
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
-            smallFist.SetActive(true);
-            smallFist.GetComponent<BoxCollider2D>().enabled = false;
-            mediumFist.SetActive(true);
-            mediumFist.GetComponent<BoxCollider2D>().enabled = false;
-            bigFist.SetActive(true);
+            _smallFist.SetActive(true);
+            _smallFist.GetComponent<BoxCollider2D>().enabled = false;
+            _mediumFist.SetActive(true);
+            _mediumFist.GetComponent<BoxCollider2D>().enabled = false;
+            _bigFist.SetActive(true);
         }
         if (Input.GetKeyUp(KeyCode.E))
         {
-            smallFist.SetActive(false);
-            smallFist.GetComponent<BoxCollider2D>().enabled = true;
-            mediumFist.SetActive(false);
-            mediumFist.GetComponent<BoxCollider2D>().enabled = true;
-            bigFist.SetActive(false);
+            _smallFist.SetActive(false);
+            _smallFist.GetComponent<BoxCollider2D>().enabled = true;
+            _mediumFist.SetActive(false);
+            _mediumFist.GetComponent<BoxCollider2D>().enabled = true;
+            _bigFist.SetActive(false);
         }
     }
 }
