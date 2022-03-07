1d4888e731985c08a86ecf345864ff053ef3e01c
Eva Luna <zap363@amerike.edu.mx>
2022-03-07 08:54:47 -0600

    Removed Redudant Scripts - Change Var Name

--- Assets/[SCRIPTS]/Movement.cs
+++ Assets/[SCRIPTS]/Movement.cs
@@ -4,26 +4,29 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float _speed = default;
     [SerializeField] private int _jumpSpeed = default;
-    [SerializeField] public GameObject _ScreenPause = default;
+    [SerializeField] public GameObject _screenPause = default;
+    [SerializeField] private KeyCode _horizontalL = default;
+    [SerializeField] private KeyCode _horizontalR = default;
+    [SerializeField] private KeyCode _jumpL = default;
+    [SerializeField] private KeyCode _pause = default;
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            Move(1);
-        }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(_horizontalL))
         {
             Move(-1);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(_horizontalR))
+        {
+            Move(1);
+        }
+        if (Input.GetKeyDown(_jumpL))
         {
             Jump();
         }
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(_pause))
         {
-            _ScreenPause.SetActive(true);
-            Time.timeScale = 0;
+            Pause();
         }
     }
 
@@ -35,5 +38,11 @@ public class Movement : MonoBehaviour
     public void Jump()
     {   
         transform.Translate(0, _jumpSpeed * _speed * Time.deltaTime, 0);
+    }
+
+    public void Pause()
+    {
+        _screenPause.SetActive(true);
+        Time.timeScale = 0;
     }
 }
