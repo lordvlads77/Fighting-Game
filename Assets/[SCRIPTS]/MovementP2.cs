a5f636c05020243a80a730e44a73beedd430518a
Eva Luna <zap363@amerike.edu.mx>
2022-03-07 07:44:18 -0600

    Changes Made

--- Assets/[SCRIPTS]/MovementP2.cs
+++ Assets/[SCRIPTS]/MovementP2.cs
@@ -5,9 +5,15 @@ public class MovementP2 : MonoBehaviour
     [SerializeField] private float _speed2 = default;
     [SerializeField] private int _jumpSpeed2 = default;
     [SerializeField] public GameObject _ScreenPause2 = default;
+    [SerializeField] private KeyCode _Horizontal1 = default;
+    [SerializeField] private KeyCode _Horizontal2 = default;
     void Update()
     {
-        if (Input.GetKey(KeyCode.Keypad4))
+        if (Input.GetKey(_Horizontal1))
+        {
+            Move2(-1);
+        }
+        if (Input.GetKey(KeyCode.Keypad7))
         {
             Move2(-1);
         }
@@ -15,7 +21,7 @@ public class MovementP2 : MonoBehaviour
         {
             Move2(1);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.Home))
         {
             Jump2();   
         }
