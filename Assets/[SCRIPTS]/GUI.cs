d3c3dc1e3ed36d8bf003286e859cfbfc93010caf
Eva Luna Álvarez <zap363@amerike.edu.mx>
2022-05-07 16:47:32 -0500

    Finished UI Nice

--- Assets/[SCRIPTS]/GUI.cs
+++ Assets/[SCRIPTS]/GUI.cs
@@ -4,8 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class GUI : MonoBehaviour
 {
+    [Header("Menu Panels")]
     [SerializeField] private GameObject _playScreen = default;
     [SerializeField] private Movement _movement = default;
+    [SerializeField] private GameObject _settingsScreen = default;
+    [SerializeField] private GameObject _keybindsScreen = default;
+    [SerializeField] private GameObject _player1KeysPanel = default;
+    [SerializeField] private GameObject _player2KeysPanel = default;
 
     void Start()
     {
@@ -27,5 +32,65 @@ public class GUI : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Settings()
+    {
+        _playScreen.SetActive(false);
+        _settingsScreen.SetActive(true);
+    }
+
+    public void Keybinds()
+    {
+        _settingsScreen.SetActive(false);
+        _keybindsScreen.SetActive(true);
+    }
+
+    public void returnToStart()
+    {
+        _settingsScreen.SetActive(false);
+        _playScreen.SetActive(true);
+    }
+
+    public void Player1Keybinds()
+    {
+        _keybindsScreen.SetActive(false);
+        _player1KeysPanel.SetActive(true);
+    }
+
+    public void Player2Keybinds()
+    {
+        _keybindsScreen.SetActive(false);
+        _player2KeysPanel.SetActive(true);
+    }
+
+    public void returnToSettings()
+    {
+        _keybindsScreen.SetActive(false);
+        _settingsScreen.SetActive(true);
+    }
+
+    public void returnTokeybindsP1()
+    {
+        _player1KeysPanel.SetActive(false);
+        _keybindsScreen.SetActive(true);
+    }
+
+    public void returnTokeybindsP2()
+    {
+        _player2KeysPanel.SetActive(false);
+        _keybindsScreen.SetActive(true);
+    }
+
+    public void okeyReturntoHomeP1()
+    {
+        _player1KeysPanel.SetActive(false);
+        _playScreen.SetActive(true);
+    }
+
+    public void okeyReturntoHomeP2()
+    {
+        _player2KeysPanel.SetActive(false);
+        _playScreen.SetActive(true);
     }
 }
