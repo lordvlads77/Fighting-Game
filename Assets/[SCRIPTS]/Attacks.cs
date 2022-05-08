bd26eacdd391c367d89b22c2342798cf1089a4ad
Eva Luna Álvarez <zap363@amerike.edu.mx>
2022-05-07 21:26:05 -0500

    Particle System Finished

--- Assets/[SCRIPTS]/Attacks.cs
+++ Assets/[SCRIPTS]/Attacks.cs
@@ -15,6 +15,20 @@ public class Attacks : MonoBehaviour
      [SerializeField] private GameObject _Playercol = default;
      [SerializeField] private KeyCode _blockMove = default;
      private Animator anim = default;
+     [Header("Light Punch Particles")]
+     [SerializeField] private GameObject _hitEffectLightPunch = default;
+     [SerializeField] private Transform _leftHandsie = default;
+     [Header("Medium Punch Particles")]
+     [SerializeField] private GameObject _hitEffetMP = default;
+     [SerializeField] private Transform _RightHandise = default;
+     [Header("Hard Punch Particles")]
+     [SerializeField] private GameObject _hitEffectHP = default;
+     [Header("Block Particles")]
+     [SerializeField] private GameObject _EffectBlock = default;
+     [SerializeField] private Transform _headsie = default;
+     [Header("Combo Particles")]
+     [SerializeField] private GameObject _EffectCombo = default;
+     [SerializeField] private Transform _legsie = default;
 
     private void Awake()
     {
@@ -26,6 +40,7 @@ public class Attacks : MonoBehaviour
         {
             _smallFist.SetActive(true);
             AnimationController.Instance.LightPunch(anim);
+            GameObject lighthit = Instantiate(_hitEffectLightPunch, _leftHandsie.transform.position, _leftHandsie.transform.rotation);
         }
         if (Input.GetKeyUp(_lightAttack))
         {
@@ -35,6 +50,7 @@ public class Attacks : MonoBehaviour
         {
             _ComboFist.SetActive(true);
             AnimationController.Instance.ComboPunch(anim);
+            GameObject vfxcombo = Instantiate(_EffectCombo, _legsie.transform.position, _legsie.transform.rotation);
         }
         if (Input.GetKeyUp(_ComboAttack))
         {
@@ -46,7 +62,9 @@ public class Attacks : MonoBehaviour
             _smallFist.GetComponent<BoxCollider>().enabled = false;
             _mediumFist.SetActive(true);
             AnimationController.Instance.MediumPuch(anim);
-            AnimationController.Instance.MediumPuch(anim);
+            GameObject mediumHit = Instantiate(_hitEffetMP, _RightHandise.transform.position,
+                _RightHandise.transform.rotation);
+
         }
         if (Input.GetKeyUp(_mediumAttack))
         {
@@ -62,6 +80,8 @@ public class Attacks : MonoBehaviour
             _mediumFist.GetComponent<BoxCollider>().enabled = false;
             _bigFist.SetActive(true);
             AnimationController.Instance.HardPunch(anim);
+            GameObject hardHit = Instantiate(_hitEffectHP, _leftHandsie.transform.position,
+                _leftHandsie.transform.rotation);
         }
         if (Input.GetKeyUp(_hardAttack))
         {
@@ -75,6 +95,7 @@ public class Attacks : MonoBehaviour
         {
             _blockFist.SetActive(true);
             AnimationController.Instance.Block(anim);
+            GameObject block = Instantiate(_EffectBlock, _headsie.transform.position, _headsie.transform.rotation);
         }
         if (Input.GetKeyUp(_blockMove))
         {
