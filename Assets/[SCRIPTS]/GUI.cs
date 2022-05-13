76a7049518639c0ae5b1fa710706e8baa3c44e6a
Eva Luna <zap363@amerike.edu.mx>
2022-05-12 21:36:04 -0500

    Some Optimizations + Particles

--- Assets/[SCRIPTS]/GUI.cs
+++ Assets/[SCRIPTS]/GUI.cs
@@ -2,99 +2,102 @@ using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GUI : MonoBehaviour
+namespace KnightBrawlers
 {
-    [Header("Menu Panels")]
-    [SerializeField] private GameObject _playScreen = default;
-    [SerializeField] private Movement _movement = default;
-    [SerializeField] private GameObject _settingsScreen = default;
-    [SerializeField] private GameObject _keybindsScreen = default;
-    [SerializeField] private GameObject _player1KeysPanel = default;
-    [SerializeField] private GameObject _player2KeysPanel = default;
-    [SerializeField] private AudioSource _lobbyMusic = default;
-    [SerializeField] private AudioSource _battleMusic = default;
-
-    void Start()
+    public class GUI : MonoBehaviour
     {
-        Time.timeScale = 0;
-    }
+        [Header("Menu Panels")]
+        [SerializeField] private GameObject _playScreen = default;
+        [SerializeField] private Movement _movement = default;
+        [SerializeField] private GameObject _settingsScreen = default;
+        [SerializeField] private GameObject _keybindsScreen = default;
+        [SerializeField] private GameObject _player1KeysPanel = default;
+        [SerializeField] private GameObject _player2KeysPanel = default;
+        [SerializeField] private AudioSource _lobbyMusic = default;
+        [SerializeField] private AudioSource _battleMusic = default;
 
-    public void ButtonPlay()
-    {
-        _playScreen.SetActive(false);
-        _movement._screenPause.SetActive(false);
-        Time.timeScale = 1;
-        _lobbyMusic.enabled = false;
-        _battleMusic.enabled = true;
-    }
+        void Start()
+        {
+            Time.timeScale = 0;
+        }
 
-    public void ButtonAgain()
-    {
-        SceneManager.LoadScene(0);
-    }
+        public void ButtonPlay()
+        {
+            _playScreen.SetActive(false);
+            _movement._screenPause.SetActive(false);
+            Time.timeScale = 1;
+            _lobbyMusic.enabled = false;
+            _battleMusic.enabled = true;
+        }
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
+        public void ButtonAgain()
+        {
+            SceneManager.LoadScene(0);
+        }
 
-    public void Settings()
-    {
-        _playScreen.SetActive(false);
-        _settingsScreen.SetActive(true);
-    }
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
 
-    public void Keybinds()
-    {
-        _settingsScreen.SetActive(false);
-        _keybindsScreen.SetActive(true);
-    }
+        public void Settings()
+        {
+            _playScreen.SetActive(false);
+            _settingsScreen.SetActive(true);
+        }
 
-    public void returnToStart()
-    {
-        _settingsScreen.SetActive(false);
-        _playScreen.SetActive(true);
-    }
+        public void Keybinds()
+        {
+            _settingsScreen.SetActive(false);
+            _keybindsScreen.SetActive(true);
+        }
 
-    public void Player1Keybinds()
-    {
-        _keybindsScreen.SetActive(false);
-        _player1KeysPanel.SetActive(true);
-    }
+        public void returnToStart()
+        {
+            _settingsScreen.SetActive(false);
+            _playScreen.SetActive(true);
+        }
 
-    public void Player2Keybinds()
-    {
-        _keybindsScreen.SetActive(false);
-        _player2KeysPanel.SetActive(true);
-    }
+        public void Player1Keybinds()
+        {
+            _keybindsScreen.SetActive(false);
+            _player1KeysPanel.SetActive(true);
+        }
 
-    public void returnToSettings()
-    {
-        _keybindsScreen.SetActive(false);
-        _settingsScreen.SetActive(true);
-    }
+        public void Player2Keybinds()
+        {
+            _keybindsScreen.SetActive(false);
+            _player2KeysPanel.SetActive(true);
+        }
 
-    public void returnTokeybindsP1()
-    {
-        _player1KeysPanel.SetActive(false);
-        _keybindsScreen.SetActive(true);
-    }
+        public void returnToSettings()
+        {
+            _keybindsScreen.SetActive(false);
+            _settingsScreen.SetActive(true);
+        }
 
-    public void returnTokeybindsP2()
-    {
-        _player2KeysPanel.SetActive(false);
-        _keybindsScreen.SetActive(true);
-    }
+        public void returnTokeybindsP1()
+        {
+            _player1KeysPanel.SetActive(false);
+            _keybindsScreen.SetActive(true);
+        }
 
-    public void okeyReturntoHomeP1()
-    {
-        _player1KeysPanel.SetActive(false);
-        _playScreen.SetActive(true);
-    }
+        public void returnTokeybindsP2()
+        {
+            _player2KeysPanel.SetActive(false);
+            _keybindsScreen.SetActive(true);
+        }
 
-    public void okeyReturntoHomeP2()
-    {
-        _player2KeysPanel.SetActive(false);
-        _playScreen.SetActive(true);
+        public void okeyReturntoHomeP1()
+        {
+            _player1KeysPanel.SetActive(false);
+            _playScreen.SetActive(true);
+        }
+
+        public void okeyReturntoHomeP2()
+        {
+            _player2KeysPanel.SetActive(false);
+            _playScreen.SetActive(true);
+        }
     }
 }
