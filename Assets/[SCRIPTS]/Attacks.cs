da0beba083b80478173278a4ab68dc0258cb0080
Eva Luna Álvarez <81182279+lordvlads77@users.noreply.github.com>
2022-05-18 00:26:39 -0500

    Revert "Fix & enhance UI"

--- Assets/[SCRIPTS]/Attacks.cs
+++ Assets/[SCRIPTS]/Attacks.cs
@@ -82,6 +82,10 @@ public class Attacks : MonoBehaviour
         }
         if (Input.GetKeyDown(_hardAttack))
         {
+            _smallFist.SetActive(true);
+            _smallFist.GetComponent<BoxCollider>().enabled = false;
+            _mediumFist.SetActive(true);
+            _mediumFist.GetComponent<BoxCollider>().enabled = false;
             _bigFist.SetActive(true);
             AnimationController.Instance.HardPunch(anim);
             GameObject hardHit = Instantiate(_hitEffectHP, _leftHandsie.transform.position,
@@ -90,6 +94,10 @@ public class Attacks : MonoBehaviour
         }
         if (Input.GetKeyUp(_hardAttack))
         {
+            _smallFist.SetActive(false);
+            _smallFist.GetComponent<BoxCollider>().enabled = true;
+            _mediumFist.SetActive(false);
+            _mediumFist.GetComponent<BoxCollider>().enabled = true;
             _bigFist.SetActive(false);
         }
         if (Input.GetKeyDown(_blockMove))
