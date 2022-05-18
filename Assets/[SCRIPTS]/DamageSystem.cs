ed87aa43a11623574bf0072e100c3b3852042273
Eva Luna Álvarez <81182279+lordvlads77@users.noreply.github.com>
2022-05-18 00:09:27 -0500

    Revert "I fixed the Jump Move yet again! "

--- Assets/[SCRIPTS]/DamageSystem.cs
+++ Assets/[SCRIPTS]/DamageSystem.cs
@@ -19,21 +19,7 @@ public class DamageSystem : MonoBehaviour
     [SerializeField] private AudioClip _lightHittSFX = default;
     [SerializeField] private AudioClip _medHitSfx = default;
     [SerializeField] private AudioClip _hardHittSfx = default;
-    [SerializeField] private AudioClip _kickSfx = default;
-    [Header("Light Punch Particles")]
-    [SerializeField] private GameObject _hitEffectLightPunch = default;
-    [SerializeField] private Transform _leftHandsie = default;
-    [Header("Medium Punch Particles")]
-    [SerializeField] private GameObject _hitEffetMP = default;
-    [SerializeField] private Transform _RightHandise = default;
-    [Header("Hard Punch Particles")]
-    [SerializeField] private GameObject _hitEffectHP = default;
-    [Header("Block Particles")]
-    [SerializeField] private GameObject _EffectBlock = default;
-    [SerializeField] private Transform _headsie = default;
-    [Header("Combo Particles")]
-    [SerializeField] private GameObject _EffectCombo = default;
-    [SerializeField] private Transform _legsie = default;
+    [SerializeField] private AudioClip _kickSfx = default; 
 
     private void Start()
     {
@@ -65,27 +51,21 @@ public class DamageSystem : MonoBehaviour
             case "smallFist":
                 MinusLife(1);
                 _punchesSFX.PlayOneShot(_lightHittSFX, 1f);
-                GameObject lighthit = Instantiate(_hitEffectLightPunch, _leftHandsie.transform.position, _leftHandsie.transform.rotation);
                 break;
             case "mediumFist":
                 MinusLife(2);
                 _punchesSFX.PlayOneShot(_medHitSfx, 1f);
-                GameObject mediumHit = Instantiate(_hitEffetMP, _RightHandise.transform.position,
-                _RightHandise.transform.rotation);
                 break;
             case "bigFist":
                 MinusLife(3);
                 _punchesSFX.PlayOneShot(_hardHittSfx, 1f);
-                GameObject hardHit = Instantiate(_hitEffectHP, _leftHandsie.transform.position,_leftHandsie.transform.rotation);
                 break;
             case "block":
                 MinusLife(0);
-                GameObject block = Instantiate(_EffectBlock, _headsie.transform.position, _headsie.transform.rotation);
                 break;
             case "ComboFist":
                 MinusLife(5);
                 _punchesSFX.PlayOneShot(_kickSfx, 1f);
-                GameObject vfxcombo = Instantiate(_EffectCombo, _legsie.transform.position, _legsie.transform.rotation);
                 break;
             default:
                 break;
